ff716189b3d1447e00f83b35931ddcb5a0d5592b
Rhythm121 <103057878+Rhythm121@users.noreply.github.com>
2023-07-20 19:12:43 +0530

    Add files via upload

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Cart.css";
+const Cart = () => {
+    return (
+
+        <section className='Cart'>
+        <div className='container'>
+          <div className='section-title'>
+            <h2>About</h2>
+          </div>
+          </div>
+          </section>
+        )
+    }
+    
+    export default Cart
